ddc5733d14308c78f2c6fba384294a1cce2d8e39
Nathan Furnal <nathan@adimian.com>
2024-02-11 17:26:07 +0100

    add read time

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import footnotes from "https://deno.land/x/lume_markdown_plugins@v0.7.0/footnote
 import favicon from "lume/plugins/favicon.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import date from "lume/plugins/date.ts";
+import readInfo from "lume/plugins/reading_info.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 
@@ -19,6 +20,7 @@ const site = lume({
   .use(title())
   .use(footnotes())
   .use(date())
+  .use(readInfo())
   .use(favicon())
   .use(sitemap())
   .use(codeHighlight())
